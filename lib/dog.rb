38c5313f8f70dd8416d609cd2c0ef59ebd41ffe3
Roxanne Gutierrez <roxannenguyenn@gmail.com>
2023-04-29 21:29:11 -0500

    commit tree 1f71be7cb38d686c88543f0e1beab9a34a40de6f

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+ 
+    #1st method: set the dog's name
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    #2nd method: get dog's name
+    def name
+        @this_dogs_name
+    end
 
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
